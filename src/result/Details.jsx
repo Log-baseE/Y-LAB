eeca28226a2f6a94296dee5d328a4f9b597e2860
NickyLogan <laurentiusdominick@yahoo.com>
2018-12-10 07:39:45 +0700

    Add save video functionality

--- src/result/Details.jsx
+++ src/result/Details.jsx
@@ -4,19 +4,52 @@ import VideoDetails from './VideoDetails';
 import ResultDetails from './ResultDetails';
 import { Button, Grid } from "@material-ui/core";
 
+const electron = window.require("electron");
+const { remote } = electron;
+const fs = remote.require("fs");
+const path = remote.require("path");
+const { dialog } = remote;
+
 const styles = theme => ({
   action: {
     paddingRight: theme.spacing.unit * 2,
   },
-  // save: {
-  // },
-  submit: {
+  save: {
     marginLeft: "auto",
-    // marginRight: theme.spacing.unit
-  }
+    marginRight: theme.spacing.unit
+  },
+  // submit: {
+  //   marginRight: theme.spacing.unit
+  // }
 });
 
 class Details extends Component {
+  handleSave = event => {
+    dialog.showSaveDialog(
+      remote.getCurrentWindow(),
+      {
+        defaultPath: "*/result.mp4",
+        filters: [{ name: "MP4 video", extensions: ["mp4"] }]
+      },
+      function(fileName) {
+        if (fileName === undefined) {
+          console.log("save cancelled");
+          return;
+        }
+        let src = path.join(remote.app.getAppPath(), "./.ylab/out_video.mp4");
+        fs.copyFile(src, fileName, err => {
+          if (err) {
+            alert(
+              "An error occurred while creating the file. Please see logs for details"
+            );
+            console.log(err);
+          }
+          alert(`Video saved to ${fileName}`);
+        })
+      }
+    );
+  };
+
   render() {
     const { classes } = this.props;
     const { state } = this.props;
@@ -31,6 +64,15 @@ class Details extends Component {
         key={'videodetails'} 
       />,
       <Grid container className={classes.action}>
+        <Button
+          component="span"
+          className={classes.save}
+          color="default"
+          onClick={this.handleSave}
+          disabled={state.file === null}
+        >
+          Save video
+        </Button>
         <Button
           variant="outlined"
           component="span"
