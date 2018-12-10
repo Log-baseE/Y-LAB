80699e444c48f18ed02c91ef17a0ac4799b37aff
NickyLogan <laurentiusdominick@yahoo.com>
2018-12-10 07:40:18 +0700

    Reformat code

--- src/main.js
+++ src/main.js
@@ -1,18 +1,25 @@
 const electron = require('electron');
-const { app, BrowserWindow, Menu, ipcMain } = require('electron');
+const {
+  app,
+  BrowserWindow,
+  dialog,
+  Menu
+} = require('electron');
 
 const path = require('path');
 const url = require('url');
-const { is } = require('electron-util');
+const {
+  is
+} = require('electron-util');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow, splash;
 
 const startUrl = process.env.ELECTRON_START_URL || url.format({
-    pathname: path.join(__dirname, '/../build/index.html'),
-    protocol: 'file:',
-    slashes: true
+  pathname: path.join(__dirname, '/../build/index.html'),
+  protocol: 'file:',
+  slashes: true
 });
 // const startUrl = url.format({
 //     pathname: path.join(__dirname, '/../main.html'),
@@ -21,99 +28,155 @@ const startUrl = process.env.ELECTRON_START_URL || url.format({
 // });
 
 const splashUrl = url.format({
-    pathname: path.join(__dirname, '../public/splash.html'),
-    protocol: 'file:',
-    slashes: true
+  pathname: path.join(__dirname, '../public/splash.html'),
+  protocol: 'file:',
+  slashes: true
 });
 
 function createLoadingScreen() {
-    splash = new BrowserWindow({
-        height: 300,
-        width: 500,
-        transparent: true,
-        frame: false,
-    })
+  splash = new BrowserWindow({
+    height: 300,
+    width: 500,
+    transparent: true,
+    frame: false,
+  })
 
-    splash.loadURL(splashUrl);
-    
-    splash.on('closed', () => {
-        splash = null
-    })
+  splash.loadURL(splashUrl);
 
-    splash.webContents.on('did-finish-load', () => {
-        splash.show();
-        splash.setProgressBar(2);
-        mainWindow.setProgressBar(2);
-    })
+  splash.on('closed', () => {
+    splash = null
+  })
+
+  splash.webContents.on('did-finish-load', () => {
+    splash.show();
+    splash.setProgressBar(2);
+    mainWindow.setProgressBar(2);
+  })
 }
 
 function createWindow() {
-    // Create the browser window.
-    mainWindow = new BrowserWindow({
-        minWidth: 800, 
-        minHeight: 600,
-        webPreferences: {
-            webSecurity: !is.development
-        },
-        backgroundColor: '#272729',
-        title: 'Y-LAB',
-        show: false,
-    });
+  // Create the browser window.
+  mainWindow = new BrowserWindow({
+    minWidth: 800,
+    minHeight: 600,
+    webPreferences: {
+      webSecurity: !is.development
+    },
+    backgroundColor: '#272729',
+    title: 'Y-LAB',
+    show: false,
+  });
 
-    // and load the index.html of the app.
-    mainWindow.loadURL(startUrl);
+  // and load the index.html of the app.
+  mainWindow.loadURL(startUrl);
 
+  mainWindow.maximize();
+
+  mainWindow.hide();
+
+  // Open the DevTools.
+  mainWindow.webContents.openDevTools();
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function () {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null
+  });
+
+  mainWindow.once('ready-to-show', () => {
+    splash.setProgressBar(-1);
+    mainWindow.setProgressBar(-1);
     mainWindow.maximize();
+  });
 
-    mainWindow.hide();
+  mainWindow.webContents.once('did-finish-load', () => {
+    mainWindow.show();
 
-    // Open the DevTools.
-    mainWindow.webContents.openDevTools();
+    if (splash) {
+      splash.close();
+    }
+  });
 
-    // Emitted when the window is closed.
-    mainWindow.on('closed', function () {
-        // Dereference the window object, usually you would store windows
-        // in an array if your app supports multi windows, this is the time
-        // when you should delete the corresponding element.
-        mainWindow = null
-    });
+  const template = [{
+      label: 'File',
+      submenu: [{
+        role: 'quit'
+      }]
+    },
+    {
+      role: 'help',
+      submenu: [{
+          label: 'Quick manual',
+          click() {
 
-    mainWindow.once('ready-to-show', () => {
-        splash.setProgressBar(-1);
-        mainWindow.setProgressBar(-1);
-        mainWindow.maximize(); 
-    });
+          }
+        },
+        {
+          label: 'Documentation',
+          click() {
+            require('electron').shell.openExternal('https://github.com/Log-baseE/Y-LAB/')
+          }
+        },
+        {
+          label: 'Replay tutorial',
+          click() {
 
-    mainWindow.webContents.once('did-finish-load', () => {
-        mainWindow.show();
+          }
+        },
+        {
+          type: 'separator',
+        },
+        {
+          label: 'Report errors',
+          click() {
+            require('electron').shell.openExternal('https://github.com/Log-baseE/Y-LAB/issues/new')
+          }
+        },
+        {
+          type: 'separator',
+        },
+        {
+          label: 'About',
+          click() {
+            dialog.showMessageBox(mainWindow, {
+              type: 'info',
+              title: 'About',
+              message: 'About message',
+            })
+          },
+        },
+      ]
+    }
+  ]
 
-        if(splash) {
-            splash.close();
-        }
-    });
+
+  const menu = Menu.buildFromTemplate(template)
+  Menu.setApplicationMenu(menu)
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.on('ready', () => {
-    createLoadingScreen();
-    createWindow();
+  createLoadingScreen();
+  createWindow();
 });
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
-    // On OS X it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform !== 'darwin') {
-        app.quit()
-    }
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
 });
 
 app.on('activate', function () {
-    // On OS X it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (mainWindow === null) {
-        createWindow()
-    }
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) {
+    createWindow()
+  }
 })
