4e82aeef2a7b34be067ad9834c3d47817e71f9ee
NickyLogan <laurentiusdominick@yahoo.com>
2019-01-04 19:26:47 +0700

    Add json functionality

--- engine/videodetector/object.py
+++ engine/videodetector/object.py
@@ -5,7 +5,7 @@ import asyncio
 import cv2
 import numpy as np
 import os
-
+import json
 
 class ObjectDetector:
     def __init__(self, verbose_level=0):
@@ -102,14 +102,13 @@ class ObjectDetector:
         self._frame_count += 1
         return result
 
-    def _write_to_video(self, coords, src_vid, dest_dir):
+    def _write_to_video(self, src_vid, dest_path):
         cap = cv2.VideoCapture(src_vid)
         width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
         height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
         fourcc = cv2.VideoWriter_fourcc(*'XVID')
-        result_dir = os.path.join(dest_dir, "out_video.avi")
 
-        out = cv2.VideoWriter(result_dir, fourcc, 30.0,
+        out = cv2.VideoWriter(dest_path, fourcc, 30.0,
                               (int(cap.get(3)), int(cap.get(4))))
 
         if self.verbose_level == 1:
@@ -118,12 +117,11 @@ class ObjectDetector:
             print("Rendering video...")
 
         ret = True
-        for i in range(self.num_frames):
+        for result in self._results:
             ret, frame = cap.read()
-            coord = self._process_frame_result(coords[i])
             if self._options.roi:
                 frame = self._draw_roi(frame, self._options.roi)
-            frame = self._draw_bounding_box(frame, coord)
+            frame = self._draw_bounding_box(frame, result)
             out.write(frame)
 
         cap.release()
@@ -132,6 +130,42 @@ class ObjectDetector:
             print("WRITE_END")
         elif self.verbose_level >= 2:
             print("Video finished rendering")
+
+    def _postprocess_results(self):
+        temp_results = [self._process_frame_result(frame_result) for frame_result in self._results]
+        return temp_results
+
+    def _write_to_json(self, dest_path):
+        labels = set()
+        total_objects = 0
+        serializable_frames = []
+        for result in self._results:
+            total_objects += len(result)
+            temp_result = []
+            for box in result:
+                left, top = box['topleft']['x'], box['topleft']['y']
+                right, bottom = box['bottomright']['x'], box['bottomright']['y']
+                label = box['label']
+                conf = box['confidence']
+                labels.add(label)
+                temp_result.append({
+                    "label": label,
+                    "confidence": float('%.2f' % conf),
+                    "topleft": {"x" : left, "y": top},
+                    "bottomright": {"x" : right, "y": bottom}
+                })
+
+            serializable_frames.append(temp_result)
+        
+        ser = {
+            "objects": list(labels),
+            "frames": serializable_frames,
+            "count_per_frame": float('%.2f' % (total_objects / self._frame_count)),
+            "type": "default"
+        }
+
+        with open(dest_path, 'w') as f:
+            json.dump(ser, f)
 
     async def _video_detect(self, videopath, dest_dir):
         num_frames = self._count_frames(videopath)
@@ -143,10 +177,16 @@ class ObjectDetector:
             print("Frames to be processed: %d frame(s)" % num_frames)
         self.num_frames = num_frames
 
-        coords = [self._process_frame(frame) for frame in self._generate_frames(
+        self._results = [self._process_frame(frame) for frame in self._generate_frames(
             videopath, self._options.roi)]
 
-        self._write_to_video(coords, src_vid=videopath, dest_dir=dest_dir)
+        self._results = self._postprocess_results()
+
+        vid_path = os.path.join(dest_dir, "out_video.avi")
+        data_path = os.path.join(dest_dir, "data.json")
+
+        self._write_to_video(src_vid=videopath, dest_path=vid_path)
+        self._write_to_json(dest_path=data_path)
 
     def detect(self, videopath: str, destination_directory: str, options: DetectorOptions) -> Tuple[str, str]:
         self._options = options
@@ -165,3 +205,5 @@ class ObjectDetector:
             print("DETECT_END")
         elif self.verbose_level >= 2:
             print("Detection finished")
+
+        # return (self.result_path)
